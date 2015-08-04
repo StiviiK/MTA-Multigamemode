23d538c939daf1e4d75448836aa0ad172e89d319
StiviiK <stefan.kuerzeder@gmail.com>
2015-08-04 11:44:00 +0200

    Shorten getElementBehindCursor function

--- multigm/client/utils.lua
+++ multigm/client/utils.lua
@@ -1,6 +1,4 @@
 function getElementBehindCursor(worldX, worldY, worldZ)
-    local x, y, z = getCameraMatrix()
-    local hit, hitX, hitY, hitZ, element = processLineOfSight(x, y, z, worldX, worldY, worldZ, false, true, true, true, false)
-
+    local hit, hitX, hitY, hitZ, element = processLineOfSight(getCamera():getPosition(), worldX, worldY, worldZ, false, true, true, true, false)
     return element
 end
