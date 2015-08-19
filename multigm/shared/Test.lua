bdd75edfe6e1975b5919d9f8a1dab7c685551617
StiviiK <stefan.kuerzeder@gmail.com>
2015-08-19 13:43:32 +0200

    Added Thread Class  --> For Example: Running heavy loops, with out lags -> See: test.lua

---
+++ multigm/shared/Test.lua
@@ -0,0 +1,49 @@
+local thisThread
+local withThread = false
+
+local lel = function ()
+  local counter = 0
+  for i = 1, 100000 do
+    print(i)
+
+    counter = counter + 1
+    if counter == 1000 then
+      print("STOP!")
+      counter = 0
+
+      if withThread then
+        Thread:pause()
+      end
+    end
+  end
+end
+
+addCommandHandler("testThread", function (cmd, play, arg)
+  if arg == "true" then
+    withThread = true
+  else
+    withThread = false
+  end
+
+  if withThread then
+    outputDebug("Running Loop with Thread")
+    local start = getTickCount()
+
+    -- With Threads
+    thisThread = Thread:new(lel)
+    --thisThread:setPiority(THREAD_PIORITY_HIGHEST)
+    thisThread:start()
+
+    outputDebug(("Server executed this after %sms."):format(getTickCount()-start))
+    outputDebug("The Server can while running the loop, do other stuff.")
+  else
+    outputDebug("Running Loop without Thread")
+    local start = getTickCount()
+
+    -- Without Threads
+    lel()
+
+    outputDebug(("Server executed this after %sms."):format(getTickCount()-start))
+    outputDebug("The Server cannot while running the loop, do other stuff.")
+  end
+end)
---
+++ multigm/shared/classes/Thread.lua
@@ -0,0 +1,66 @@
+Thread = inherit(Object)
+Thread.Map = {}
+Thread.Piority = {
+  ["highest"] = 50;
+  ["high"] = 150;
+  ["middle"] = 250;
+  ["low"] = 500;
+}
+
+function Thread:constructor(func)
+  self.m_Id = table.push(Thread.Map, self)
+  self.m_Func = func
+  self.m_Piority = THREAD_PIORITY_LOW
+  self.ms_Thread = false
+  self.ms_Timer = false
+end
+
+function Thread:destructor()
+  Thread.Map[self:getId()] = nil
+  self.ms_Thread = nil
+
+  if isTimer(self.ms_Timer) then
+    killTimer(self.ms_Timer)
+  end
+end
+
+function Thread:start()
+  self.ms_Thread = coroutine.create(self.m_Func)
+  self:resume()
+
+  self.ms_Timer = setTimer(function()
+    if self:getStatus() == COROUTINE_STATUS_SUSPENDED then
+      self:resume()
+    elseif self:getStatus() == COROUTINE_STATUS_DEAD then
+      delete(self)
+    end
+  end, Thread.Piority[self.m_Piority], -1)
+end
+
+function Thread:resume()
+  return coroutine.resume(self:getThread())
+end
+
+function Thread:pause()
+  coroutine.yield()
+end
+
+function Thread:getStatus()
+  return coroutine.status(self:getThread())
+end
+
+function Thread:getId()
+  return self.m_Id
+end
+
+function Thread:getPiority()
+  return self.m_Piority
+end
+
+function Thread:getThread()
+  return self.ms_Thread
+end
+
+function Thread:setPiority(piority)
+  self.m_Piority = piority
+end
