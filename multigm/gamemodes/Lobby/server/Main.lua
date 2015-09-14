ae38369d84332175526d3972cf42a8841819caa9
StiviiK <stefan.kuerzeder@gmail.com>
2015-09-14 14:33:49 +0200

    Worked a bit on the GameRooms

--- multigm/gamemodes/Lobby/server/Main.lua
+++ multigm/gamemodes/Lobby/server/Main.lua
@@ -1,6 +1,15 @@
 function Lobby:constructor()
   addRemoteEvents{"onLobbyDownloadFinished"}
   addEventHandler("onLobbyDownloadFinished", root, bind(Lobby.onDownloadComplete, self))
+
+  -- Create GameRooms
+  self:addRoom("Lobby #1", 15)
+  self:addRoom("Lobby #2", 15)
+  self:addRoom("Lobby #3", 15)
+  self:addRoom("Lobby #4", 15)
+
+    -- Fallback Room
+    self:addRoom("Lobby - FALLBACK ROOM")
 end
 
 function Lobby:destructor()
@@ -19,4 +28,35 @@ function Lobby:onDownloadComplete()
   client:setPosition(spawn.Position)
   client:setRotation(0, 0, spawn.Rotation)
   client:setInterior(spawn.Interior)
+
+  local Id = self:findFreeRoom()
+  if Id ~= -1 then
+    self:addToRoom(Id, client)
+  else
+    client:onInternalError(("%s-#%s"):format(self:getName(), RUNTIME_ERROR_NO_ROOM))
+  end
+end
+
+function Lobby:findFreeRoom()
+  local Id = 0
+  while true do
+    Id = Id + 1
+
+    if Id > #self.m_Rooms then
+      Id = -1
+      break;
+    end
+
+    if self.m_Rooms[Id] then
+      if self.m_Rooms[Id].maxplayers == -1 then
+        break;
+      else
+        if #self.m_Rooms[Id].players < self.m_Rooms[Id].maxplayers then
+          break;
+        end
+      end
+    end
+  end
+
+  return Id
 end
