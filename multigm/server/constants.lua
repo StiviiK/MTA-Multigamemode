4942d5022ed170abae1bf82565a3ad13c65cfb94
StiviiK <stefan.kuerzeder@gmail.com>
2015-12-12 12:35:17 +0100

    MySQL fixes

--- multigm/server/constants.lua
+++ multigm/server/constants.lua
@@ -1,8 +1,8 @@
-MYSQL_HOST	= "localhost"
+MYSQL_HOST	= "127.0.0.1"
 MYSQL_PORT	= 3306
 MYSQL_USER	= "root"
-MYSQL_PW	  = ""
-MYSQL_DB	  = "multigm_develop"
+MYSQL_PW	  = "Lvi8NdDmSDy6gwg"
+MYSQL_DB	  = "multigm"
 
 USE_REMOTE_API = false
 API_URL = "stivik.jusonex.net/multigm/api"
