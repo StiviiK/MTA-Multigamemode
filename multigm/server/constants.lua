d6c3aa84cb4de13f3868ed988ccee4fd3d364f88
StiviiK <stefan.kuerzeder@gmail.com>
2015-12-12 12:43:06 +0100

    MySQL fix #3

--- multigm/server/constants.lua
+++ multigm/server/constants.lua
@@ -1,9 +1,10 @@
-MYSQL_HOST	= "127.0.0.1"
+MYSQL_HOST	= "localhost"
 MYSQL_PORT	= 3306
 MYSQL_USER	= "root"
 MYSQL_PW	  = "Lvi8NdDmSDy6gwg"
 MYSQL_DB	  = "multigm"
 
+IS_TESTSERVER = getServerName():find("Testserver") ~= nil
 USE_REMOTE_API = false
 API_URL = "stivik.jusonex.net/multigm/api"
 VERSION_URL = "stivik.jusonex.net/multigm/version"
