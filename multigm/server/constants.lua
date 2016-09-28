95e734a9897cb1296517c21227eda867b3a38344
StiviiK <stefan.kuerzeder@gmail.com>
2016-09-28 13:16:07 +0200

    Update MySQL DB

--- multigm/server/constants.lua
+++ multigm/server/constants.lua
@@ -1,10 +1,11 @@
-MYSQL_HOST	= "127.0.0.1"
+MYSQL_HOST	= "localhost"
 MYSQL_PORT	= 3306
 MYSQL_USER	= "root"
-MYSQL_PW	  = "Lvi8NdDmSDy6gwg"
-MYSQL_DB	  = "multigm"
+MYSQL_PW	= ""
+MYSQL_DB	= "multigm"
 
-IS_TESTSERVER = getServerName():find("Testserver") ~= nil
+--IS_TESTSERVER = getServerName():find("Testserver") ~= nil
+IS_TESTSERVER = true
 USE_REMOTE_API = false
 API_URL = "stivik.jusonex.net/multigm/api"
 VERSION_URL = "stivik.jusonex.net/multigm/version"
